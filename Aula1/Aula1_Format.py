c45021d46a4fea18763eb0f15229a790255da5d5
Natália Doreto <dorettto@icloud.com>
2025-04-16 15:12:53 -0300

    Aula 1

---
+++ Aula1/Aula1_Format.py
@@ -0,0 +1,6 @@
+A = 14
+B = 32
+S1 = 'Valores: A é {} e B é {}'.format(A, B)
+S2 =  f'Valores: A é {A} e B é {B}'
+print(S1)
+print(S2)
---
+++ Aula1/Aula1_Input.py
@@ -0,0 +1,6 @@
+A = input('Digite algo aqui: ')
+print(A)
+N1 = int(input('Converta essa entrada em um inteiro: '))
+N2 = int(input('Insira um segundo inteiro: '))
+B = N1 + N2
+print(B)
