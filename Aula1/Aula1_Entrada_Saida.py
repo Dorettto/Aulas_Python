56dac707cfe00408e6b6b681f2585105a626922b
Natália Doreto <dorettto@icloud.com>
2025-04-16 14:47:52 -0300

    Aula 1

---
+++ Aula1/Aula1_Entrada_Saida.py
@@ -0,0 +1,7 @@
+print ('Hello World')
+print ('O Python usa aspas simples')
+print ('E quebras as linhas sozinho')
+X = 32
+Y = 89
+print (X, Y)
+print ('Valores: W = {0} e Z = {1}'.format(X, Y))
---
+++ Aula1/Aula1_Separadores.py
@@ -0,0 +1,8 @@
+A = 12
+B = 19
+print(A, B)
+print(A, B, sep='-')
+print(A, B, sep='<-->')
+print(A, B, sep=', ')
+print('Quebra\nde\nLinhas\n')
+print('Esta é\tuma tabulação')
