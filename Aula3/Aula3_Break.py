f961ea166f837cefd8e43af092477bd7b0ba9953
Natália Doreto <dorettto@icloud.com>
2025-04-23 22:34:02 -0300

    Exercícios da Aula 3

---
+++ Aula3/Aula3_Break.py
@@ -0,0 +1,9 @@
+x = 1
+
+while True:
+    x = int(input('Digite um número: '))
+    if x == 0:
+        break
+    print(x)
+
+print('Fim do Programa')
---
+++ Aula3/Aula3_Comando_Laco.py
@@ -0,0 +1,8 @@
+print('Inicio do Programa')
+
+cont = 1
+while cont <= 4:
+    print(cont)
+    cont = cont + 1
+
+print('Fim do Programa')
