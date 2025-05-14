074cbaf81b1d9010a4d89265c4b7dfcba61a2bac
Natália Doreto <dorettto@icloud.com>
2025-05-14 17:54:35 -0300

    [feat] Inclusão de exercícios de Lista

---
+++ Aula4/Aula04_ConceitoListas.py
@@ -0,0 +1,11 @@
+L = [1, 2, 3, 4, 5]
+
+print('Tamanho da lista: ')
+print(len(L))
+
+print('Exibição da lista completa: ')
+i = 0
+while i < len(L):
+    print(L[i], end=' ')
+    i += 1
+
---
+++ Aula4/Aula04_ManipulacaoListas.py
@@ -0,0 +1,13 @@
+A = []
+A.append(1)
+A.append(2)
+A.append(3)
+A.append(4)
+A.append(5)
+print(A)
+
+del A [0]
+print(A)
+
+A[0] = 999
+print(A)
